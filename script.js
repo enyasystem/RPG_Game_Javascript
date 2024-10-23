f03dd76e0a62f21ce67738982d619095f39c75ae
enyaelvis <enyaelvis@gmail.com>
2024-10-23 18:11:42 +0100

    Attack function updated

--- script.js
+++ script.js
@@ -182,6 +182,8 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monster[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;;
+
 }
 
 function dodge() {
